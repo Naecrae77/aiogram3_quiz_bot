6b1e52a14226050c07e843ede033ba9803a93bac
Samuel Oluleye <seun4united@mail.ru>
2024-10-14 20:17:17 +1000

    Update functions.py

--- aiogram3 quiz bot/functions.py
+++ aiogram3 quiz bot/functions.py
@@ -197,16 +197,6 @@ async def cmd_quiz(message: types.Message):
     # Launching a new quiz
     await new_quiz(message)
 
-
-async def create_table():
-    # Create a connection to the database (if it does not exist, it will be created)
-    async with aiosqlite.connect('quiz_bot.db') as db:
-        # Execute an SQL query to the database
-        await db.execute('''CREATE TABLE IF NOT EXISTS quiz_state (user_id INTEGER PRIMARY KEY, question_index INTEGER)''')
-        await db.execute('''CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, score INTEGER)''')       
-        # Save changes
-        await db.commit()
-
 #help handler
 @dp.message(Command('help'))
 async def cmd_start(message: types.Message):
